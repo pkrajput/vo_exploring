dfdc588f80e414ab29fbf17f2663c54bf4e6bbc0
Vladimir Chernyy <ghost23rus@gmail.com>
2022-05-22 09:43:44 +0200

    successfully running train

--- DL_VO/scripts/train_resnet50_pose_256.sh
+++ DL_VO/scripts/train_resnet50_pose_256.sh
@@ -1,6 +1,7 @@
-DATA_ROOT=/media/bjw/Disk
-TRAIN_SET=$DATA_ROOT/Dataset/kitti_vo_256/
-python train.py $TRAIN_SET \
+DATA_ROOT=../../../datasets/
+TRAIN_SET=$DATA_ROOT/kitti_vo_256/
+
+python3 ../train.py $TRAIN_SET \
 --resnet-layers 50 \
 --num-scales 1 \
 -b4 -s0.1 -c0.5 --epoch-size 1000 --sequence-length 3 \
@@ -9,4 +10,7 @@ python train.py $TRAIN_SET \
 --with-auto-mask 1 \
 --with-pretrain 1 \
 --log-output \
---name resnet50_pose_256
+--name resnet50_pose_256 \
+--with-coord-conv 1 \
+--conv1-weight-mode random \
+--epochs 1
