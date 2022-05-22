95f5636a3b01aa531674e671c6769422b06b8a46
Vladislav Trifonov <63505267+Vlad382@users.noreply.github.com>
2022-05-22 13:11:23 +0300

    Add files via upload
    scripts to run experiments with CoordConv

---
+++ DL_VO/scripts/train_pose_allrandom_first_all.sh
@@ -0,0 +1,28 @@
+DATA_ROOT=../..
+TRAIN_SET=$DATA_ROOT/kitti_vo_256
+
+python3 ../train.py $TRAIN_SET \
+--resnet-layers 50 \
+--num-scales 1 \
+-b4 -s0.1 -c0.5 --epoch-size 1000 --sequence-length 3 \
+--with-ssim 1 \
+--with-mask 1 \
+--with-auto-mask 1 \
+--with-pretrain 0 \
+--pretrained-disp //PATH// \
+--pretrained-pose //PATH// \
+--log-output //PATH?// \
+--epochs 100 \
+--lr 1e-3 \
+--name coordconv_allrandom_first_all \
+--with-coord-conv 1 \
+--conv1-weight-mode all_random \
+--fine-tune-mode first_then_all \
+--unfreeze-epoch 35 \
+--use-scheduler 1 \
+--warmup-lr 1e-5 \
+--warmup-epoch 70 \
+--step-size 10 \
+--gamma-lr .1 \
+--min-lr 1e-5 \
+--run-id $1 \
