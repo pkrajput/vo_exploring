02243ae72515ff59597a1192390738d8e2cb88d5
Vlad382 <vladtrifono@gmail.com>
2022-05-26 16:14:28 +0300

    final scripts

---
+++ DL_VO/scripts/dummy_scripts/train_pose_allrandom_first_all1.sh
@@ -0,0 +1,31 @@
+DATA_ROOT=../..
+TRAIN_SET=$DATA_ROOT/kitti_vo_256
+
+EPOCH_SIZE=1000
+
+python3 ../train.py $TRAIN_SET \
+--resnet-layers 50 \
+--num-scales 1 \
+-b4 -s0.1 -c0.5 --epoch-size $EPOCH_SIZE --sequence-length 3 \
+--with-ssim 1 \
+--with-mask 1 \
+--with-auto-mask 1 \
+--with-pretrain 0 \
+--pretrained-disp ../checkpoints_pretrained/dispnet_model_best.pth.tar \
+--pretrained-pose ../checkpoints_pretrained/exp_pose_model_best.pth.tar \
+--epochs 50 \
+--lr 1e-5 \
+--name coordconv_allrandom_first_all \
+--with-coord-conv 1 \
+--conv1-weight-mode all_random \
+--fine-tune-mode first_then_all \
+--unfreeze-epoch 15 \
+--use-scheduler 1 \
+--run-id $1 \
+--warmup-lr 1e-8 \
+--warmup-epoch 1 \
+--step-size 30 \
+--gamma-lr .1 \
+--min-lr 1e-10 \
+--print-freq 50 \
+--epoch-size-val 10
--- DL_VO/scripts/dummy_scripts/train_pose_random_whole.sh
+++ DL_VO/scripts/dummy_scripts/train_pose_random_whole.sh
@@ -1,28 +1,30 @@
 DATA_ROOT=../..
 TRAIN_SET=$DATA_ROOT/kitti_vo_256
 
+EPOCH_SIZE=1000
+
 python3 ../train.py $TRAIN_SET \
 --resnet-layers 50 \
 --num-scales 1 \
--b 10 -s 0.1 -c 0.5 --epoch-size 50 --sequence-length 3 \
+-b4 -s0.1 -c0.5 --epoch-size $EPOCH_SIZE --sequence-length 3 \
 --with-ssim 1 \
 --with-mask 1 \
 --with-auto-mask 1 \
 --with-pretrain 0 \
 --pretrained-disp ../checkpoints_pretrained/dispnet_model_best.pth.tar \
 --pretrained-pose ../checkpoints_pretrained/exp_pose_model_best.pth.tar \
---epochs 100 \
+--epochs 50 \
 --lr 1e-5 \
---name coordconv_allrandom_first_all \
+--name coordconv_random_whole \
 --with-coord-conv 1 \
---conv1-weight-mode all_random \
---fine-tune-mode first_then_all \
---unfreeze-epoch 35 \
---use-scheduler 0 \
+--conv1-weight-mode random \
+--fine-tune-mode whole \
+--use-scheduler 1 \
 --run-id $1 \
---print-freq 1
-# --warmup-lr 1e-5 \
-# --warmup-epoch 70 \
-# --step-size 10 \
-# --gamma-lr .1 \
-# --min-lr 1e-5 \
+--warmup-lr 1e-10 \
+--warmup-epoch 2 \
+--step-size 10 \
+--gamma-lr .1 \
+--min-lr 1e-10 \
+--print-freq 50 \
+--epoch-size-val 10
